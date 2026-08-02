b8dbc19c0e3b2c6384bcac67f663ac968433437b
petrosyanahit0109 <anahitpetrosyan0109@gmail.com>
2026-08-02 21:05:00 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Set the date we're counting down to (June 14, 2026 at 14:30)
+const countDownDate = new Date("June 14, 2026 14:30:00").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days < 10 ? "0" + days : days;
+    document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
+    document.getElementById("minutes").innerText = minutes < 10 ? "0" + minutes : minutes;
+    document.getElementById("seconds").innerText = seconds < 10 ? "0" + seconds : seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "<h2>Մենք արդեն ամուսնացած ենք!</h2>";
+    }
+}, 1000);
